7d7373e1a966402803b0469eb4c85015f2d73f20
rcarreraamoedo <55793088+rcarreraamoedo@users.noreply.github.com>
2019-12-11 13:12:01 +0100

    Add files via upload

---
+++ boletin12_2/Boletin12_2.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin12_2;
+
+/**
+ *
+ * @author usuario
+ */
+public class Boletin12_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Clase1 obj1 = new Clase1(5, 4);
+        System.out.print(obj1.modificar(4) + " ");
+        Clase1 obj2 = new Clase1(5, 4);
+        System.out.print(obj2.modificar(5) + " ");
+        obj2 = obj1;
+        System.out.println(obj2.modificar(5) + " ");
+    }//fin main 
+}
+
+class Clase1 {
+
+    int p1, p2;
+
+    public Clase1(int i, int j) {
+        p1 = i;
+        p2 = j;
+    }
+
+    public int modificar(int i) {
+        p1 = p1 + i;
+        p2 = p2 + i;
+        System.out.print(p2 + " ");
+        return p1;
+    }
+    
+}
